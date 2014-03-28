69be760d16aa8696ec700ca27603dcc4de69c335
tsukky528 <e.tsukiyama@aktsk.jp>
2014-03-28 19:58:15 +0900

    もろもろ

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,5 @@
+require 'file_size_validator'
+
 class Training < ActiveRecord::Base
   has_many :relationships, foreign_key:"like_movie_id", dependent: :destroy
 
@@ -8,7 +10,9 @@ class Training < ActiveRecord::Base
   	errors.add(:movie, "どちらか動画が必要です。")
   	errors.add(:youtube, "どちらか動画が必要です。")
   end
-  
+
+  validates :movie, file_size: {maximum: 10.megabytes.to_i}
+
   validates :explain, :muscle, presence: true
 
 
